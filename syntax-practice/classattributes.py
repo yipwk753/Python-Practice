26d7e26240e4994c5b9c318688ac90a8c3e8c479
William <yipwk753@gmail.com>
2020-10-04 16:45:58 -0400

    Practicing working w/ class attributes

---
+++ syntax-practice/classattributes.py
@@ -0,0 +1,15 @@
+class A:
+    a = "class attribute"
+
+x = A()
+y = A()
+#A, x, and y all have the same id
+print("x id:", id(x.a))
+print("y id:", id(y.a))
+print("A id:", id(A.a))
+x.a = "new instance attribute"
+A.a = "Class attribute changed."
+#A and y have the same id, x has a different id
+print("x id:", id(x.a))
+print("y id:", id(y.a))
+print("A id:", id(A.a))
